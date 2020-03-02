e4bb978d09583b6a61be66adb01a9c9578220993
M23081998 <manojarumugam2308@gmail.com>
2020-03-02 12:24:00 +0530

    updated

--- src/main/java/com/chainsys/ebusapp/controller/busController.java
+++ src/main/java/com/chainsys/ebusapp/controller/busController.java
@@ -1,30 +1,33 @@
 package com.chainsys.ebusapp.controller;
 
+import java.util.ArrayList;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
 import BusDetails.busDetails;
 import BusDetails.busDetailsDAO;
 import BusDetails.busDetailsDAOImpl;
-
+import SearchBus.FindBus;
+import SearchBus.FindBusDAO;
+import SearchBus.FindBusDAOImpl;
 
 @RestController
 @RequestMapping("api")
 public class busController {
-	
-	busDetailsDAO a=new busDetailsDAOImpl();
-	
-	@PostMapping("/buses")
-	public void addBus(@RequestParam("bus_id") Integer bid,@RequestParam("bus_name") String busname,
-			@RequestParam("from_location") String fromLocation,@RequestParam("to_location") String toLocation,
-			@RequestParam("journey_date") String date,@RequestParam("ticket_price") Integer tprice,@RequestParam("travelling_timr") String travelTime,
-			@RequestParam("maximun_seats") Integer maximumSeats,@RequestParam("available_seats") Integer availableSeats)
-	{
-		busDetails b=new busDetails();
+
+	busDetailsDAO a = new busDetailsDAOImpl();
+
+	@PostMapping("/addbuses")
+	public void addBus(@RequestParam("bus_id") Integer bid, @RequestParam("bus_name") String busname,
+			@RequestParam("from_location") String fromLocation, @RequestParam("to_location") String toLocation,
+			@RequestParam("journey_date") String date, @RequestParam("ticket_price") Integer tprice,
+			@RequestParam("travelling_time") String travelTime, @RequestParam("maximun_seats") Integer maximumSeats,
+			@RequestParam("available_seats") Integer availableSeats) {
+		busDetails b = new busDetails();
 
 		b.setBusId(bid);
 		b.setBusName(busname);
@@ -37,12 +40,27 @@ public class busController {
 		b.setAvailableSeats(availableSeats);
 		try {
 			a.addBus(b);
-			
+
 		} catch (Exception e) {
-			
+
 			e.printStackTrace();
 		}
 	}
-		
-}
 
+	@GetMapping("/searchbuses")
+	public ArrayList<FindBus> searchbus(@RequestParam("from_location") String fromLocation,
+			@RequestParam("to_location") String toLocation, @RequestParam("journey_date") String date) {
+
+		FindBusDAO dao = new FindBusDAOImpl();
+		ArrayList<FindBus> a = new ArrayList<FindBus>();
+		try {
+			a = dao.searchbus(fromLocation, toLocation, date);
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+		return a;
+
+	}
+}
